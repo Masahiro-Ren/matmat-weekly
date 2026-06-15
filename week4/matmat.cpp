c08d26bb809905ecbc5b28c6971e08a4fec800dd
Masahiro-Ren <renxzb@gmail.com>
2026-06-15 17:13:25 +0900

    Add 4x8 and 8x8 simd kernels

--- week4/matmat.cpp
+++ week4/matmat.cpp
@@ -34,12 +34,97 @@ void simd256kernel_4x4(const double* A, const double* B, double* C,
 
 }
 
+void simd512kernel_8x8(const double* A, const double* B, double* C,
+                       size_t ic, size_t jc, size_t N, size_t K)
+{
+    // Define Accumulators
+    __m512d c0 = _mm512_setzero_pd();
+    __m512d c1 = _mm512_setzero_pd();
+    __m512d c2 = _mm512_setzero_pd();
+    __m512d c3 = _mm512_setzero_pd();
+    __m512d c4 = _mm512_setzero_pd();
+    __m512d c5 = _mm512_setzero_pd();
+    __m512d c6 = _mm512_setzero_pd();
+    __m512d c7 = _mm512_setzero_pd();
+
+    // Computations
+    for(size_t k = 0; k < K; k++)
+    {
+        // Load 4 cols of B at row k
+        __m512d b = _mm512_loadu_pd(&B[k * N + jc]);
+        // Load and broadcasting values of A[ic + ir][k]
+        __m512d a0 = _mm512_set1_pd(A[(ic + 0) * K + k]);
+        __m512d a1 = _mm512_set1_pd(A[(ic + 1) * K + k]);
+        __m512d a2 = _mm512_set1_pd(A[(ic + 2) * K + k]);
+        __m512d a3 = _mm512_set1_pd(A[(ic + 3) * K + k]);
+        __m512d a4 = _mm512_set1_pd(A[(ic + 4) * K + k]);
+        __m512d a5 = _mm512_set1_pd(A[(ic + 5) * K + k]);
+        __m512d a6 = _mm512_set1_pd(A[(ic + 6) * K + k]);
+        __m512d a7 = _mm512_set1_pd(A[(ic + 7) * K + k]);
+        // Kernel computations
+        c0 = _mm512_fmadd_pd(a0, b, c0);
+        c1 = _mm512_fmadd_pd(a1, b, c1);
+        c2 = _mm512_fmadd_pd(a2, b, c2);
+        c3 = _mm512_fmadd_pd(a3, b, c3);
+        c4 = _mm512_fmadd_pd(a4, b, c4);
+        c5 = _mm512_fmadd_pd(a5, b, c5);
+        c6 = _mm512_fmadd_pd(a6, b, c6);
+        c7 = _mm512_fmadd_pd(a7, b, c7);
+    }
+
+    // Store Results
+    _mm512_storeu_pd(&C[(ic + 0) * N + jc], c0);
+    _mm512_storeu_pd(&C[(ic + 1) * N + jc], c1);
+    _mm512_storeu_pd(&C[(ic + 2) * N + jc], c2);
+    _mm512_storeu_pd(&C[(ic + 3) * N + jc], c3);
+    _mm512_storeu_pd(&C[(ic + 4) * N + jc], c4);
+    _mm512_storeu_pd(&C[(ic + 5) * N + jc], c5);
+    _mm512_storeu_pd(&C[(ic + 6) * N + jc], c6);
+    _mm512_storeu_pd(&C[(ic + 7) * N + jc], c7);
+
+}
+
+void simd256kernel_4x8(const double* A, const double* B, double* C,
+                       size_t ic, size_t jc, size_t N, size_t K)
+{
+    // Define Accumulators
+    __m256d c[MR256_4x8][2];
+    for(size_t ir = 0; ir < MR256_4x8; ir++)
+    {
+        c[ir][0] = _mm256_setzero_pd();
+        c[ir][1] = _mm256_setzero_pd();
+    }
+
+    // Computations
+    for(size_t k = 0; k < K; k++)
+    {
+        // Load 2 sets (8 cols) of B at row k
+        __m256d b0 = _mm256_loadu_pd(&B[k * N + jc]);
+        __m256d b1 = _mm256_loadu_pd(&B[k * N + jc + MR256_4x8]);
+        for(size_t ir = 0; ir < MR256_4x8; ir++)
+        {
+            // Load and broadcasting values of A[ic + ir][k]
+            __m256d a = _mm256_set1_pd(A[(ic + ir) * K + k]);
+            // Kernel computations
+            c[ir][0] = _mm256_fmadd_pd(a, b0, c[ir][0]);
+            c[ir][1] = _mm256_fmadd_pd(a, b1, c[ir][1]);
+        }
+    }
+
+    // Store Results
+    for(size_t ir = 0; ir < MR256_4x8; ir++)
+    {
+        _mm256_storeu_pd(&C[(ic + ir) * N + jc], c[ir][0]);
+        _mm256_storeu_pd(&C[(ic + ir) * N + (jc + 4)], c[ir][1]);
+    }
+}
+
 void simd512kernel_8x16(const double* A, const double* B, double* C,
                         size_t ic, size_t jc, size_t N, size_t K)
 {
     // Define Accumulators
-    __m512d c[MR_512][2];
-    for(size_t ir = 0; ir < MR_512; ir++)
+    __m512d c[MR512_8x16][2];
+    for(size_t ir = 0; ir < MR512_8x16; ir++)
     {
         c[ir][0] = _mm512_setzero_pd();
         c[ir][1] = _mm512_setzero_pd();
@@ -50,11 +135,11 @@ void simd512kernel_8x16(const double* A, const double* B, double* C,
     {
         // Load 2 sets (16 cols) of B at row k
         __m512d b0 = _mm512_loadu_pd(&B[k * N + jc]);
-        __m512d b1 = _mm512_loadu_pd(&B[k * N + jc + MR_512]);
+        __m512d b1 = _mm512_loadu_pd(&B[k * N + jc + MR512_8x16]);
 
-        for(size_t ir = 0; ir < MR_512; ir++)
+        for(size_t ir = 0; ir < MR512_8x16; ir++)
         {
-            // Load and broad casting values of A[ic + ir][k]
+            // Load and broadcasting values of A[ic + ir][k]
             __m512d a = _mm512_set1_pd(A[(ic + ir) * K + k]);
             // Kernel Computations
             c[ir][0] = _mm512_fmadd_pd(a, b0, c[ir][0]);
@@ -63,31 +148,55 @@ void simd512kernel_8x16(const double* A, const double* B, double* C,
     }
 
     // Store reuslts
-    for(size_t ir = 0; ir < MR_512; ir++)
+    for(size_t ir = 0; ir < MR512_8x16; ir++)
     {
         _mm512_storeu_pd(&C[(ic + ir) * N + jc], c[ir][0]);
         _mm512_storeu_pd(&C[(ic + ir) * N + (jc + 8)], c[ir][1]);
     }
 }
 
-void matmat_simd256(const double* A, const double* B, double* C,
-                    size_t M, size_t N, size_t K)
+void matmat_simd256_4x4(const double* A, const double* B, double* C,
+                        size_t M, size_t N, size_t K)
 {
-    for(size_t i = 0; i < M; i += MR_256)
+    for(size_t i = 0; i < M; i += MR256_4x4)
     {
-        for(size_t j = 0; j < N; j += NR_256)
+        for(size_t j = 0; j < N; j += NR256_4x4)
         {
             simd256kernel_4x4(A, B, C, i, j, N, K);
         }
     }
 }
 
-void matmat_simd512(const double* A, const double* B, double* C,
+void matmat_simd256_4x8(const double* A, const double* B, double* C,
+                        size_t M, size_t N, size_t K)
+{
+    for(size_t i = 0; i < M; i += MR256_4x8)
+    {
+        for(size_t j = 0; j < N; j += NR256_4x8)
+        {
+            simd256kernel_4x8(A, B, C, i, j, N, K);
+        }
+    }
+}
+
+void matmat_simd512_8x8(const double* A, const double* B, double* C,
+                        size_t M, size_t N, size_t K)
+{
+    for(size_t i = 0; i < M; i += MR512_8x8)
+    {
+        for(size_t j = 0; j < N; j += NR512_8x8)
+        {
+            simd512kernel_8x8(A, B, C, i, j, N, K);
+        }
+    }
+}
+
+void matmat_simd512_8x16(const double* A, const double* B, double* C,
                     size_t M, size_t N, size_t K)
 {
-    for(size_t i = 0; i < M; i += MR_512)
+    for(size_t i = 0; i < M; i += MR512_8x16)
     {
-        for(size_t j = 0; j < N; j += NR_512)
+        for(size_t j = 0; j < N; j += NR512_8x16)
         {
             simd512kernel_8x16(A, B, C, i, j, N, K);
         }
--- week4/matmat.h
+++ week4/matmat.h
@@ -5,26 +5,39 @@
 #include <immintrin.h>
 
 // Kernel size for AVX2 (256bit)
-constexpr size_t MR_256 = 4;
-constexpr size_t NR_256 = 4;
+constexpr size_t MR256_4x4 = 4;
+constexpr size_t NR256_4x4 = 4;
+
+constexpr size_t MR256_4x8 = 4;
+constexpr size_t NR256_4x8 = 8;
 
 // Kernel size for AVX512 (512bit)
-constexpr size_t MR_512 = 8;
-constexpr size_t NR_512 = 16;
+constexpr size_t MR512_8x8 = 8;
+constexpr size_t NR512_8x8 = 8;
+
+constexpr size_t MR512_8x16 = 8;
+constexpr size_t NR512_8x16 = 16;
 
 // The kernels are hand-unrolled for these exact tile shapes; changing a
 // constant without rewriting the matching kernel would be a silent bug.
-static_assert(MR_256 == 4 && NR_256 == 4,
-              "simd256kernel is hard-coded for a 4x4 (MR_256 x NR_256) tile "
-              "(4 accumulators c0..c3, one __m256d = 4 doubles per row)");
-static_assert(NR_512 == 16,
-              "simd512kernel is hard-coded for 16 columns (2 x __m512d of 8 doubles)");
+static_assert(MR256_4x4 == 4 && NR256_4x4 == 4,
+              "simd256kernel_4x4 hard-codes a 4x4 tile (4 accumulators c0..c3, one __m256d = 4 cols)");
+static_assert(MR256_4x8 == 4 && NR256_4x8 == 8,
+              "simd256kernel_4x8 hard-codes 8 cols (2 x __m256d); the b1 column offset uses MR256_4x8, so it must be 4");
+static_assert(MR512_8x8 == 8 && NR512_8x8 == 8,
+              "simd512kernel_8x8 hard-codes an 8x8 tile (8 accumulators c0..c7, one __m512d = 8 cols)");
+static_assert(MR512_8x16 == 8 && NR512_8x16 == 16,
+              "simd512kernel_8x16 hard-codes 16 cols (2 x __m512d); the b1 column offset uses MR512_8x16, so it must be 8");
 
-void matmat_simd256(const double* A, const double* B, double* C,
-                    size_t M, size_t N, size_t K);
+void matmat_simd256_4x4(const double* A, const double* B, double* C,
+                        size_t M, size_t N, size_t K);
+void matmat_simd256_4x8(const double* A, const double* B, double* C,
+                        size_t M, size_t N, size_t K);
 
-void matmat_simd512(const double* A, const double* B, double* C,
-                    size_t M, size_t N, size_t K);
+void matmat_simd512_8x8(const double* A, const double* B, double* C,
+                        size_t M, size_t N, size_t K);
+void matmat_simd512_8x16(const double* A, const double* B, double* C,
+                         size_t M, size_t N, size_t K);
 
 // ------------------- For reference -----------------------
 void matmat_ikj(const double* A, const double* B, double* C, size_t M, size_t N, size_t K);
